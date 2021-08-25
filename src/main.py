ff4a74261bb654e9dce78e3aead9eac8b95f75be
palapapa <palapapa93@gmail.com>
2021-08-25 21:56:13 +0800

    add: -n & --no-auto-correct options & use lookup_compound instead
    Added the -n and --no-auto-correct options to disable auto-correct and
    changed from using lookup to lookup_compound to auto-correct.

--- src/main.py
+++ src/main.py
@@ -51,12 +51,12 @@ def main() -> None:
         dest="output_path"
     )
     argument_parser.add_argument(
-        "-a",
-        "--auto-correct",
-        action="store_true",
+        "-n",
+        "--no-auto-correct",
+        action="store_false",
         required=False,
-        help="Whether to use auto-correct.",
-        default=False,
+        help="Whether to disable auto-correct.",
+        default=True,
         dest="auto_correct"
     )
     arguments: argparse.Namespace = argument_parser.parse_args()
@@ -64,7 +64,7 @@ def main() -> None:
     image_paths: list[str]
     (images, image_paths) = load_images(arguments.input_paths)
     result: str = str()
-    before_auto_correct: list[list[str]] = list(list())
+    before_auto_correct: list[str] = list()
     after_auto_correct: list[list[symspellpy.symspellpy.SuggestItem]] = list(list())
     symspell: symspellpy.SymSpell = symspellpy.SymSpell()
     dictionary_path: str = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
@@ -74,29 +74,22 @@ def main() -> None:
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     for i in range(len(images)):
         try:
-            before_auto_correct.append(str(pytesseract.image_to_string(images[i])).split(" "))
+            before_auto_correct.append(str(pytesseract.image_to_string(images[i])))
         except Exception as e:
             print(f"{image_paths[i]} cannot be opened.({str(e)})")
     if arguments.auto_correct:
         def auto_correct() -> None:
-            for paragraph in before_auto_correct:
-                corrected_paragraph: list[symspellpy.symspellpy.SuggestItem] = list()
-                for word in paragraph:
-                    try:
-                        corrected_paragraph.append(symspell.lookup(word, symspellpy.symspellpy.Verbosity.TOP)[0])
-                    except:
-                        corrected_paragraph.append(symspellpy.symspellpy.SuggestItem(word, None, None))
-                after_auto_correct.append(corrected_paragraph)
+            for before in before_auto_correct:
+                suggestion: list[symspellpy.symspellpy.SuggestItem] = symspell.lookup_compound(before, max_edit_distance=2, transfer_casing=True)
+                after_auto_correct.append(suggestion)
         auto_correct() # Python why are you like this?????? Why can't a loop have its own scope?????
-        for paragraph_suggestion in after_auto_correct:
-            for word_suggestion in paragraph_suggestion:
-                result += word_suggestion.term + " "
-            result += "\n"
+        for suggestions in after_auto_correct:
+            paragraph: str = str()
+            for suggestion in suggestions:
+                paragraph += suggestion.term + " "
+            result += paragraph + "\n"
     else:
-        for paragraph in before_auto_correct:
-            for word in paragraph:
-                result += word + " "
-            result += "\n"
+        result = "\n".join(before_auto_correct)
     try:
         with open(arguments.output_path, "a+") as output:
             output.write(result)
