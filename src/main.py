b478b022a1f3a7dd59b200336ff22b8921e3fdf4
palapapa <palapapa93@gmail.com>
2021-08-26 20:09:32 +0800

    modify: lookup_compound to lookup
    Changed from using lookup_compound to lookup as it proved to be more
    useful.

--- src/main.py
+++ src/main.py
@@ -67,31 +67,41 @@ def main() -> None:
     for image in images:
         image = cv2.cvtColor(numpy.array(image), cv2.COLOR_BGR2GRAY)
     result: str = str()
-    before_auto_correct: list[str] = list()
+    before_auto_correct: list[list[str]] = list(list())
     after_auto_correct: list[list[symspellpy.symspellpy.SuggestItem]] = list(list())
     symspell: symspellpy.SymSpell = symspellpy.SymSpell()
+    dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
     bigram_path: str = pkg_resources.resource_filename("symspellpy", "frequency_bigramdictionary_en_243_342.txt")
+    symspell.load_dictionary(dictionary_path, term_index=0, count_index=1)
     symspell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     for i in range(len(images)):
         try:
-            before_auto_correct.append(str(pytesseract.image_to_string(images[i])))
+            before_auto_correct.append(str(pytesseract.image_to_string(images[i])).split(" "))
         except Exception as e:
             print(f"{image_paths[i]} cannot be opened.({str(e)})")
     if arguments.auto_correct:
         def auto_correct() -> None:
-            for before in before_auto_correct:
-                suggestion: list[symspellpy.symspellpy.SuggestItem] = symspell.lookup_compound(before, max_edit_distance=2, transfer_casing=True)
-                after_auto_correct.append(suggestion)
+            for paragraph in before_auto_correct:
+                corrected_paragraph: list[symspellpy.symspellpy.SuggestItem] = list()
+                for word in paragraph:
+                    try:
+                        corrected_paragraph.append(symspell.lookup(word, symspellpy.symspellpy.Verbosity.TOP)[0])
+                    except:
+                        corrected_paragraph.append(symspellpy.symspellpy.SuggestItem(word, None, None))
+                after_auto_correct.append(corrected_paragraph)
         auto_correct() # Python why are you like this?????? Why can't a loop have its own scope?????
-        for suggestions in after_auto_correct:
-            paragraph: str = str()
-            for suggestion in suggestions:
-                paragraph += suggestion.term + " "
-            paragraph = paragraph.strip()
-            result += paragraph + "\n"
+        for paragraph_suggestion in after_auto_correct:
+            for word_suggestion in paragraph_suggestion:
+                result += word_suggestion.term + " "
+            result = result.strip()
+            result += "\n"
     else:
-        result = "\n".join(before_auto_correct)
+        for paragraph in before_auto_correct:
+            for word in paragraph:
+                result += word + " "
+            result = result.strip()
+            result += "\n"
     try:
         with open(arguments.output_path[0], "a+") as output:
             output.write(result)
